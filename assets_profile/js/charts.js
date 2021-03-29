bb387e01973145f707e7e337109b11133c3f7f7f
PabloRodriguezGarrido11 <pabrodgar9@alum.us.es>
2021-03-29 21:31:24 +0200

    Añadidas nuevas funcionalidades en el perfil de médico

--- assets_profile/js/charts.js
+++ assets_profile/js/charts.js
@@ -1,28 +1,171 @@
 
 
-var char1 = document.getElementById('myChart1').getContext('2d');
-var myChart1 = new Chart(char1, {
+var comparativaSemanal = document.getElementById('comparativaSemanal').getContext('2d');
+var comparativaSemanal = new Chart(comparativaSemanal, {
     type: 'bar',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6' ,'Día 7'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: ['Correctas'],
+            data: [5,4,4,5,7,4,1],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+
+               
+            ],
+            borderWidth: 1
+        },{
+            label: ['Incorrectas'],
+            data: [2,1,1,4,2,1,0],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+
+            ],
+            borderWidth: 1
+        }
+
+        
+    ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+
+var comparativaSemana2 = document.getElementById('comparativaSemana2').getContext('2d');
+var comparativaSemana2 = new Chart(comparativaSemana2, {
+    type: 'radar',
+    data: {
+        labels: ['MEMORIA','ATENCION','PERCEPCION','CALCULO','LENGUAJE'],
+        datasets: [{
+            label: ['Correctas'],
+            data: [5,4,4,5,7],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                
+
+               
+            ],
+            borderWidth: 1
+        },{
+            label: ['Incorrectas'],
+            data: [2,1,1,4,2],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                
+
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+              
+
+            ],
+            borderWidth: 1 
+        }
+
+        
+    ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    
+                }
+            }]
+        } 
+        
+    }
+});
+
+
+
+
+
+var testSemanal = document.getElementById('testSemanal').getContext('2d');
+
+var testSemanal = new Chart(testSemanal, {
+    type: 'line',
+    data: {
+        labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6' ,'Día 7'],
+        datasets: [{
+            label: 'Puntos obtenidos por día',
+            data: [8,5,7,6,8,6,7],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+
+                
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+               
             ],
             borderWidth: 1
         }]
@@ -38,30 +181,392 @@ var myChart1 = new Chart(char1, {
     }
 });
 
-var char2 = document.getElementById('myChart2').getContext('2d');
 
-var myChart2 = new Chart(char2, {
+
+
+
+
+
+
+var comparativaMensual1 = document.getElementById('comparativaMensual1').getContext('2d');
+var comparativaMensual1 = new Chart(comparativaMensual1, {
+    type: 'bar',
+    data: {
+        labels: ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4'],
+        datasets: [{
+            label: ['Correctas'],
+            data: [10,11,10,12],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+               
+
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+               
+
+               
+            ],
+            borderWidth: 1
+        },{
+            label: ['Incorrectas'],
+            data: [4,5,2,5],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+               
+
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+              
+
+            ],
+            borderWidth: 1
+        }
+
+        
+    ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+
+var comparativaMensual2 = document.getElementById('comparativaMensual2').getContext('2d');
+var comparativaMensual2 = new Chart(comparativaMensual2, {
+    type: 'radar',
+    data: {
+        labels: ['MEMORIA','ATENCION','PERCEPCION','CALCULO','LENGUAJE'],
+        datasets: [{
+            label: ['Correctas'],
+            data: [13,14,15,12,11],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                
+
+               
+            ],
+            borderWidth: 1
+        },{
+            label: ['Incorrectas'],
+            data: [4,5,2,5,6],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                
+
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(255, 99, 132, 1)',
+              
+
+            ],
+            borderWidth: 1 
+        }
+
+        
+    ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true,
+                    
+                }
+            }]
+        } 
+        
+    }
+});
+
+
+
+
+
+var testMensual = document.getElementById('testMensual').getContext('2d');
+
+var testMensual = new Chart(testMensual, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Semana 1', 'Semana 2', 'Semana 3', 'Semana 4'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Puntos obtenidos por semana',
+            data: [10,11,12,13],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+               
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+var memoria = document.getElementById('memoria').getContext('2d');
+
+var memoria = new Chart(memoria, {
+    type: 'doughnut',
+    data: {
+        labels: ['Correctas', 'Incorrectas'],
+        datasets: [{
+            label: '# of Votes',
+            data: [2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+               
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+
+
+var atencion = document.getElementById('atencion').getContext('2d');
+
+var atencion = new Chart(atencion, {
+    type: 'doughnut',
+    data: {
+        labels: ['Correctas', 'Incorrectas'],
+        datasets: [{
+            label: '# of Votes',
+            data: [1, 2],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+               
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+var percepcion = document.getElementById('percepcion').getContext('2d');
+
+var percepcion = new Chart(percepcion, {
+    type: 'doughnut',
+    data: {
+        labels: ['Correctas', 'Incorrectas'],
+        datasets: [{
+            label: '# of Votes',
+            data: [1, 1],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+               
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+
+var calculo = document.getElementById('calculo').getContext('2d');
+
+var calculo = new Chart(calculo, {
+    type: 'doughnut',
+    data: {
+        labels: ['Correctas', 'Incorrectas'],
+        datasets: [{
+            label: '# of Votes',
+            data: [1, 0],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+               
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+var lenguaje = document.getElementById('lenguaje').getContext('2d');
+
+var lenguaje = new Chart(lenguaje, {
+    type: 'doughnut',
+    data: {
+        labels: ['Correctas', 'Incorrectas'],
+        datasets: [{
+            label: '# of Votes',
+            data: [0, 0],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+               
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+
+var test = document.getElementById('test').getContext('2d');
+
+var test = new Chart(test, {
+    type: 'line',
+    data: {
+        labels: ['Pregunta 1', 'Pregunta 2', 'Pregunta 3', 'Pregunta 4'],
+        datasets: [{
+            label: 'Puntos obtenidos por pregunta',
+            data: [1, 3, 2, 2],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+
+                
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+               
             ],
             borderWidth: 1
         }]
